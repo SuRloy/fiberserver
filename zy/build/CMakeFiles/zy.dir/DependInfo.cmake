a5dcd0a0882fe307007d6d9960be5ca07ec78a02
SuRloy <1184114761@qq.com>
2023-11-27 02:17:45 +0800

    2023/11/27

--- zy/build/CMakeFiles/zy.dir/DependInfo.cmake
+++ zy/build/CMakeFiles/zy.dir/DependInfo.cmake
@@ -1,22 +1,16 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
-  )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/zouyang/webserver/zy/zy/config.cc" "/home/zouyang/webserver/zy/build/CMakeFiles/zy.dir/zy/config.cc.o"
-  "/home/zouyang/webserver/zy/zy/log.cc" "/home/zouyang/webserver/zy/build/CMakeFiles/zy.dir/zy/log.cc.o"
-  "/home/zouyang/webserver/zy/zy/util.cc" "/home/zouyang/webserver/zy/build/CMakeFiles/zy.dir/zy/util.cc.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# Preprocessor definitions for this target.
-set(CMAKE_TARGET_DEFINITIONS_CXX
-  "zy_EXPORTS"
   )
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ss/workspace/zy/zy/config.cc" "CMakeFiles/zy.dir/zy/config.cc.o" "gcc" "CMakeFiles/zy.dir/zy/config.cc.o.d"
+  "/home/ss/workspace/zy/zy/log.cc" "CMakeFiles/zy.dir/zy/log.cc.o" "gcc" "CMakeFiles/zy.dir/zy/log.cc.o.d"
+  "/home/ss/workspace/zy/zy/util.cc" "CMakeFiles/zy.dir/zy/util.cc.o" "gcc" "CMakeFiles/zy.dir/zy/util.cc.o.d"
   )
 
 # Targets to which this target links.
