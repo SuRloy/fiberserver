8d62b63b0f0b84edb8d6da72d21497b2cb1a9443
SuRloy <1184114761@qq.com>
2023-11-27 02:19:55 +0800

    2023/11/27

---
+++ CMakeFiles/zy.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zy.dir/zy/log.cc.o"
+  "CMakeFiles/zy.dir/zy/log.cc.o.d"
+  "CMakeFiles/zy.dir/zy/util.cc.o"
+  "CMakeFiles/zy.dir/zy/util.cc.o.d"
+  "zy/lib/libzy.pdb"
+  "zy/lib/libzy.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/zy.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for zy.
+# This may be replaced when dependencies are built.
