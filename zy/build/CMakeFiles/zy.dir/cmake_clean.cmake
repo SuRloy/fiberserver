1b5fbb45c0771cd3ad7f34f0226a7c821814b41b
zouyang <1184114761@qq.com>
2023-12-23 03:05:18 +0800

    clear

---
+++ zy/build/CMakeFiles/zy.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../lib/libzy.pdb"
+  "../lib/libzy.so"
+  "CMakeFiles/zy.dir/zy/config.cc.o"
+  "CMakeFiles/zy.dir/zy/log.cc.o"
+  "CMakeFiles/zy.dir/zy/thread.cc.o"
+  "CMakeFiles/zy.dir/zy/util.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
